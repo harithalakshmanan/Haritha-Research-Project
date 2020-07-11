2ffc86afef8a0842a3c6045321ccf715feef0746
harithalakshmanan <35085883+harithalakshmanan@users.noreply.github.com>
2020-07-11 15:33:25 -0400

    Update EP300.py
    Added comments regarding gene entrez Ids

--- EP300.py
+++ EP300.py
@@ -39,18 +39,32 @@ def main():
 	print("The brca_tcga_pan_can_atlas_2018 study spans {} patients".format(len(patients)))
 
 	# select genes in the cohort of interest
-	genes = cbioportal.Genes.getGeneUsingGET(geneId='EP300').result()
-	print("The Entrez Gene ID for gene EP300 is {} ".format(genes.entrezGeneId))
+	genes = cbioportal.Genes.getGeneUsingGET(geneId='MAP3K1').result() #TP53 = 7157, EP300 = 2033, PIK3CA=5290, CDH1=999, GATA3=2625, MAP3K1=4214
+	print("The Entrez Gene ID for gene MAP3K1 is {} ".format(genes.entrezGeneId))
 
 	
 	# what kind of mutations do the patients in this cohort have? 
 	mutations = cbioportal.Mutations.getMutationsInMolecularProfileBySampleListIdUsingGET(
-    	entrezGeneId=2033,
+    	entrezGeneId=4214,
         molecularProfileId='brca_tcga_pan_can_atlas_2018_mutations',
     	sampleListId='brca_tcga_pan_can_atlas_2018_all',
     	projection='DETAILED'
 	).result()
-	print("The number of patients with a mutation of the EP300 gene is {} ".format(len(mutations)))
+	print("The number of patients with a mutation of the 7157 gene is {} ".format(len(mutations)))
+	
+	# Id stores the patientId
+	Id='TCGA-A2-A0YF'
+
+	# how many months has this patient been alive (overall survival in months)?
+	living = cbioportal.Clinical_Data.getAllClinicalDataOfPatientInStudyUsingGET(attributeId='OS_MONTHS', patientId=Id, studyId='brca_tcga_pan_can_atlas_2018').result()[0]['value']
+	print("months living {} ".format(living))
+
+	# how old is the patient?3
+	status = cbioportal.Clinical_Data.getAllClinicalDataOfPatientInStudyUsingGET(attributeId='OS_STATUS', patientId=Id, studyId='brca_tcga_pan_can_atlas_2018').result()[0]['value']
+	print("living {} ".format(status))
+
+	patients = cbioportal.Patients.getAllPatientsInStudyUsingGET(studyId='brca_tcga_pan_can_atlas_2018').result()
+	patientIds = [x.patientId for x in patients]	
 
 	# which genes have mutations? create plot to visualize
 	mutated_genes = Counter([m.gene.hugoGeneSymbol for m in mutations])
@@ -60,6 +74,9 @@ def main():
 		  len(mutated_genes)
 	))
 
+	mutation = cbioportal.Mutations.getMutationsInMolecularProfileBySampleListIdUsingGET(entrezGeneId=4214, molecularProfileId='brca_tcga_pan_can_atlas_2018_mutations', sampleListId='brca_tcga_pan_can_atlas_2018_all').result()
+	patient=[x.patientId for x in mutation]
+	print(patient)
 	plot_mutations(mutated_genes)
 
 if __name__ == '__main__':
