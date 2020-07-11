ca3fd2bdd8d444e3257907373d29e3c96c3da0c2
harithalakshmanan <35085883+harithalakshmanan@users.noreply.github.com>
2020-07-11 15:33:50 -0400

    Update 6_30.py
    Added comments regarding entrez gene Ids

--- 6_30.py
+++ 6_30.py
@@ -95,13 +95,13 @@ def main():
 	print("The brca_tcga_pan_can_atlas_2018 study spans {} patients".format(len(patients)))
 
 	# select genes in the cohort of interest
-	genes = cbioportal.Genes.getGeneUsingGET(geneId='EP300').result()
-	print("The Entrez Gene ID for gene EP300 is {} ".format(genes.entrezGeneId))
+	genes = cbioportal.Genes.getGeneUsingGET(geneId='MAP3K1').result() #TP53 = 7157, EP300 = 2033, PIK3CA=5290, CDH1=999, GATA3=2625, MAP3K1=4214
+	print("The Entrez Gene ID for gene MAP3K1 is {} ".format(genes.entrezGeneId))
 
 	
 	# what kind of mutations do the patients in this cohort have? 
 	mutations = cbioportal.Mutations.getMutationsInMolecularProfileBySampleListIdUsingGET(
-    	entrezGeneId=2033,
+    	entrezGeneId=4214,
         molecularProfileId='brca_tcga_pan_can_atlas_2018_mutations',
     	sampleListId='brca_tcga_pan_can_atlas_2018_all',
     	projection='DETAILED'
@@ -110,7 +110,7 @@ def main():
 	patients = cbioportal.Patients.getAllPatientsInStudyUsingGET(studyId='brca_tcga_pan_can_atlas_2018').result()
 	patientIds = [x.patientId for x in patients]
 
-	mutation_EP300 = cbioportal.Mutations.getMutationsInMolecularProfileBySampleListIdUsingGET(entrezGeneId=2033, molecularProfileId='brca_tcga_pan_can_atlas_2018_mutations', sampleListId='brca_tcga_pan_can_atlas_2018_all').result()
+	mutation_EP300 = cbioportal.Mutations.getMutationsInMolecularProfileBySampleListIdUsingGET(entrezGeneId=4214, molecularProfileId='brca_tcga_pan_can_atlas_2018_mutations', sampleListId='brca_tcga_pan_can_atlas_2018_all').result()
 	patient_EP300=[x.patientId for x in mutation_EP300]
 	
 	graph(patientIds,patient_EP300)
